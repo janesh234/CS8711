f90cac16eea0b737506d3b962b2e346071ba3ea1
janesh234 <142491990+janesh234@users.noreply.github.com>
2023-08-17 15:50:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from flask import Flask, render_template, request
 
-# [START gae_python38_app]
-# [START gae_python3_app]
-from flask import Flask
+# import json to load JSON data to a python dictionary
+import json
+
+# urllib.request to make a request to api
+import urllib.request
 
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
 
+@app.route('/', methods =['POST', 'GET'])
+def weather():
+	if request.method == 'POST':
+		city = request.form['city']
+	else:
+		# for default name mathura
+		city = 'mathura'
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'CS8711!'
+	# your API key will come here
+	api = api_key_here
+
+	# source contain json data from api
+	source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q =' + city + '&appid =' + api).read()
+
+	# converting JSON data to a dictionary
+	list_of_data = json.loads(source)
+
+	# data for variable list_of_data
+	data = {
+		"country_code": str(list_of_data['sys']['country']),
+		"coordinate": str(list_of_data['coord']['lon']) + ' '
+					+ str(list_of_data['coord']['lat']),
+		"temp": str(list_of_data['main']['temp']) + 'k',
+		"pressure": str(list_of_data['main']['pressure']),
+		"humidity": str(list_of_data['main']['humidity']),
+	}
+	print(data)
+	return render_template('index.html', data = data)
+
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
-
+	app.run(debug = True)
